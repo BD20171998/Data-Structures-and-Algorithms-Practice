2110a6be8e84cb09782253133b6c652679e82684
BD20171998 <robert.deprizio@gmail.com>
2020-09-17 20:23:07 -0700

    Deleted white space

--- LeetCode/C++/0058 - Length of Last Word.cpp
+++ LeetCode/C++/0058 - Length of Last Word.cpp
@@ -2,35 +2,34 @@
 //https://leetcode.com/problems/length-of-last-word/
 
 #include <string>
-class Solution {
+class Solution
+{
 public:
-    int lengthOfLastWord(string s) {
-    
-    if (s.length() == 0 || (s.length() == 1 && s[0] == 32))
-        return (0);
-    
-    if (s.length() == 1 && s[0] != 32)
-        return (1);
-        
-    int length = 0;
-   
-    for (int i = s.length()-1; i >= 0; --i) {
-  
-        
-    if ( s[i]  != 32){
-       
-        length +=1;
-        if (i-1<0 || s[i-1] == 32 )
-            break;
+    int lengthOfLastWord(string s)
+    {
+
+        if (s.length() == 0 || (s.length() == 1 && s[0] == 32))
+            return (0);
+
+        if (s.length() == 1 && s[0] != 32)
+            return (1);
+
+        int length = 0;
+
+        for (int i = s.length() - 1; i >= 0; --i)
+        {
+
+            if (s[i] != 32)
+            {
+                length += 1;
+
+                if (i - 1 < 0 || s[i - 1] == 32)
+                    break;
+            }
+
+            else if (s[i] == 32)
+                continue;
         }
-        
-     else if (s[i] == 32   )
-    
-        continue;
-        
-        
-    }
-        
-        return length;
+        return (length);
     }
 };
