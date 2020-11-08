44dba85e58f58511753f85779fa360f39f84f47b
BD20171998 <robert.deprizio@gmail.com>
2020-11-07 22:37:23 -0800

    Fixed comment section

--- LeetCode/Python/1237 - Find Positive Integer Solution for a Given Equation.py
+++ LeetCode/Python/1237 - Find Positive Integer Solution for a Given Equation.py
@@ -1,6 +1,4 @@
 '''
-
-'''
 1237. Find Positive Integer Solution for a Given Equation
 https: // leetcode.com/problems/find-positive-integer-solution-for-a-given-equation/
 '''
@@ -18,27 +16,27 @@ https: // leetcode.com/problems/find-positive-integer-solution-for-a-given-equat
 
 
 def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
-    
-    pairs = []
-    
-    def bin_search(x,l,r,z):
-        mid = l + (r - l) // 2 #(y + x) // 2 
-        
-        if l>r:
-            return 
-        
-        elif customfunction.f(x,mid) > z: 
-            bin_search(x, l,mid-1, z) 
 
-        elif customfunction.f(x,mid) < z: 
-            bin_search(x,mid + 1, r, z) 
+    pairs = []
+
+    def bin_search(x, l, r, z):
+        mid = l + (r - l) // 2  # (y + x) // 2
+
+        if l > r:
+            return
+
+        elif customfunction.f(x, mid) > z:
+            bin_search(x, l, mid-1, z)
+
+        elif customfunction.f(x, mid) < z:
+            bin_search(x, mid + 1, r, z)
 
         else:
-            pairs.append([x,mid])
+            pairs.append([x, mid])
             return
-        
-    for x in range(1,1002):
-        l,r = 1, 1001
-        bin_search(x,l,r,z)
-            
+
+    for x in range(1, 1002):
+        l, r = 1, 1001
+        bin_search(x, l, r, z)
+
     return pairs
