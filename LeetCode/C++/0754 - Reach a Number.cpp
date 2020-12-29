4ac173761d51060c05b3e9da1531b7c3fa08051e
BD20171998 <robert.deprizio@gmail.com>
2020-12-28 18:06:32 -0800

    Adding #754

---
+++ LeetCode/C++/0754 - Reach a Number.cpp
@@ -0,0 +1,28 @@
+/**
+754. Reach a Number
+https://leetcode.com/problems/reach-a-number/
+*/
+
+class Solution {
+public:
+    int reachNumber(int target) {
+        
+        target = abs(target);
+        int steps = 0;
+        int sum = 0; 
+        
+        while (sum < target)
+        {
+            steps++;
+            sum+=steps;
+        }
+            
+        while((sum-target) % 2 !=0)
+        {
+            steps++;
+            sum+=steps;
+        }
+        
+        return steps;
+    }
+};
